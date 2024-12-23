add934820d8c03b772588da7b5135bda4a37bf39
WilliamRamsey <williamdawsonramsey@gmail.com>
2024-12-22 22:01:15 -0500

    my version of auto-annotator

--- data_tools/automatic_annotator.py
+++ data_tools/automatic_annotator.py
@@ -1,8 +1,10 @@
 import os
 from ultralytics import YOLO
 import cv2
+# strictly for visualization
+from ultralytics.utils.plotting import Annotator, colors
 
-path_to_model = ""
+path_to_model = "C:/Users/willi/OneDrive/Desktop/HITNET/runs/segment/train/weights/best.pt"
 path_to_images = "C:/Users/willi/OneDrive/Desktop/HITNET/data/datasets/games1_40/YOLODataset_seg/images/val"
 # Use custom model to run first round of analysis for labelme.
 model = YOLO(path_to_model)
@@ -10,11 +12,16 @@ model = YOLO(path_to_model)
 
 for image_file in os.listdir(path_to_images):
     # Open image
-    img = cv2.imread(f"{path_to_images}z/{image_file}")
-    # Run annotator on image
+    img = cv2.imread(f"{path_to_images}/{image_file}")
+    # Run model on image
     results = model(img)
+    # for result in results:
+        # print(result.show())
+    # Display annotation mask
+    annotator = Annotator(img, line_width=1)
     # Convert annotation masks to polygons
-    print(results)
+
     # Save polygons
+    break
 
 # Open polygons with images in labelme
--- data_tools/consolidate_folders.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dir = "C:/Users/willi/OneDrive/Desktop/HITNET/data/raw/images"
-
-i = 0
-for folder in os.listdir(dir):
-    for file in os.listdir(f"{dir}/{folder}"):
-        i += 1
-        os.rename(f"{dir}/{folder}/{file}", f"C:/Users/willi/OneDrive/Desktop/HITNET/data/raw/wrapped/{i}.jpg")
