22b3fbdd9c21be590656c5922eb505ee21563c6f
WilliamRamsey <williamdawsonramsey@gmail.com>
2024-12-26 00:33:00 -0500

    WORKING AUTOANNOTATOR

--- data_tools/automatic_annotator.py
+++ data_tools/automatic_annotator.py
@@ -1,67 +1,73 @@
 import os
-from ultralytics import YOLO
+import base64
+import json
 import cv2
 from ultralytics.data.annotator import auto_annotate
-import json
 
 path_to_model = "C:/Users/willi/OneDrive/Desktop/HITNET/runs/segment/train/weights/best.pt"
 path_to_images = "C:/Users/willi/OneDrive/Desktop/HITNET/data_tools/test"
+
 # Use custom model to run first round of analysis for labelme.
 auto_annotate(  
     data="data/datasets/games1_40/YOLODataset_seg/images/val",
     det_model=path_to_model,
-    sam_model="mobile_sam.pt",
+    sam_model="C:/Users/willi/OneDrive/Desktop/HITNET/models/mobile_sam.pt",
     output_dir=f"{path_to_images}/annotations",
 )
 
+
 # convert yolo to labelme json format
 for file in os.listdir(f"{path_to_images}/annotations"):
-    # read dimensions based on origional image
-    img = cv2.imread(f"{path_to_images}/{file[:-4]}.png")
-    h, w = img.shape[:2]
+  # read dimensions based on origional image and add image byte data
+  img = cv2.imread(f"{path_to_images}/{file[:-4]}.png")
+  h, w = img.shape[:2]
+  with open(f"{path_to_images}/{file[:-4]}.png", "rb") as img_file:
+    img_binary = img_file.read()
+  encoded_binary = base64.b64encode(img_binary).decode("utf-8")
 
-    # generate json template at image level
-    output_dict = {
-        "version": "5.5.0",
+  # generate json template at image level
+  output_dict = {
+    "version": "5.5.0",
+    "flags": {},
+    "shapes": [],
+    "imagePath": f"..\\{file[:-4].replace("/", "\\")}.png",
+    "imageData": encoded_binary,
+    "imageHeight": h,
+    "imageWidth": w
+  }
+
+  # read yolo formatted data
+  with open(f"{path_to_images}/annotations/{file}") as yolo_annotation:
+    # iterate through lines in yolo annotation
+    for yolo_helmet in yolo_annotation.readlines():
+      yolo_helmet = yolo_helmet.split(" ")
+      # grab first value as class ID
+      print(yolo_helmet)
+      class_id = yolo_helmet[0]
+      # grab the x and y values as their own extended lists
+      yolo_x_list = yolo_helmet[1::2]
+      yolo_y_list = yolo_helmet[2::2]
+      points = []
+      # iterate through values, scale up, and append to list
+      for yolo_x, yolo_y in zip(yolo_x_list, yolo_y_list):
+        point = [float(yolo_x)*w, float(yolo_y)*h]
+        points.append(point)
+            
+      # ADJUTST "Helmet" to class ID for production
+      output_dict["shapes"].append({
+        "label": "Helmet",
+        "points": points,
+        "group_id": None,
+        "description": "",
+        "shape_type": "polygon",
         "flags": {},
-        "shapes": [],
-        "imagePath": f"..\\{path_to_images}\\{file[:-4]}.png",
-        "imageHeight": h,
-        "imageWidth": w
-    }
-
-    # read yolo formatted data
-    with open(f"{path_to_images}/annotations/{file}") as yolo_annotation:
-        # iterate through lines in yolo annotation
-        for yolo_helmet in yolo_annotation.readlines():
-            yolo_helmet = yolo_helmet.split(" ")
-            # grab first value as class ID
-            print(yolo_helmet)
-            class_id = yolo_helmet[0]
-            # grab the x and y values as their own extended lists
-            yolo_x_list = yolo_helmet[1::2]
-            yolo_y_list = yolo_helmet[2::2]
-            points = []
-            # iterate through values, scale up, and append to list
-            for yolo_x, yolo_y in zip(yolo_x_list, yolo_y_list):
-                point = [float(yolo_x)*w, float(yolo_y)*h]
-                points.append(point)
-            # ADJUTST "Helmet" to class ID for production
-            output_dict["shapes"].append({
-                "label": "Helmet",
-                "points": points,
-                "group_id": None,
-                "description": "",
-                "shape_type": "polygon",
-                "flags": {},
-                "mask": None
-            })
-
-        yolo_annotation.close()
-    with open(f"{path_to_images}/{file[:-4]}.json", "w") as output_json:
-        json.dump(output_dict, output_json, indent=2)
-    output_json.close()
-    # save dict as json
+        "mask": None
+      })
+  
+  
+  # save dict as json
+  with open(f"{path_to_images}/{file[:-4]}.json", "w") as output_json:
+    json.dump(output_dict, output_json, indent=2)
 
 # Format of labelme json files
 """
