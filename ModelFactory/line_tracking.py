ff293a382e1455b8ff863fd55bd077b683371123
WilliamRamsey <williamdawsonramsey@gmail.com>
2025-06-29 22:49:44 -0400

    Optimized dynamic canny threashold convergence

--- ModelFactory/line_tracking.py
+++ ModelFactory/line_tracking.py
@@ -4,23 +4,27 @@ import numpy as np
 from skimage.measure import shannon_entropy
 import matplotlib.pyplot as plt
 
-def dynamic_canny(gray_img, target_entropy = 0.2, entropy_threshold = 0.025, lower_z = 0.5, upper_z = 1):
-    blur_kernal = 7
+def dynamic_canny(gray_img, target_entropy = 0.15, entropy_threshold = 0.025, upper_z = 25, lower_z_ratio = 0.85):
+    blur_kernal = 45
+    lower_z = upper_z * lower_z_ratio
 
+    try_number = 1
     while True:
-        blured_img = cv2.GaussianBlur(gray_img, (blur_kernal, blur_kernal), 0)
+        blured_img = cv2.GaussianBlur(gray_img, (blur_kernal, blur_kernal), 0, None, 0)
 
         med_brightness = np.median(blured_img)
         brightness_sdev = np.std(blured_img)
         upper_threashold = med_brightness + (upper_z * brightness_sdev)
         lower_threashold = med_brightness + (lower_z * brightness_sdev)
+
         """
+        Color implementation
         color_chanels = cv2.split(blured_img)
         edges = [cv2.Canny(chanel, 30, 90) for chanel in color_chanels]
         combined_edges = cv2.bitwise_or(cv2.bitwise_or(edges[0], edges[1]), edges[2])
         """
         
-        canny_edges = cv2.Canny(blured_img, lower_threashold, upper_threashold)
+        canny_edges = cv2.Canny(blured_img, lower_threashold, upper_threashold, apertureSize=7)
 
         # evaluation
         density = np.count_nonzero(canny_edges) / canny_edges.size
@@ -31,41 +35,39 @@ def dynamic_canny(gray_img, target_entropy = 0.2, entropy_threshold = 0.025, low
         cdist = cv2.cvtColor(canny_edges, cv2.COLOR_GRAY2BGR)
         cv2.imshow(f"Density Adjust", cdist)
         cv2.waitKey(1)
-        
-        if entropy > (target_entropy + entropy_threshold):
-            # If thresholds are too close, increase the upper
-            upper_z += 0.02
-            lower_z += 0.02
 
-        elif entropy < (target_entropy - entropy_threshold):
-            upper_z -= 0.02
-            lower_z -= 0.02
-        
+
+        if entropy > (target_entropy + entropy_threshold) or entropy < (target_entropy - entropy_threshold):
+            # If thresholds are too close, increase the upper
+            upper_z += 500 * .5 ** (try_number - 1) * (entropy - target_entropy)
+            lower_z = lower_z_ratio * upper_z
+
         else:
-            return canny_edges, lower_z, upper_z
+            return canny_edges, upper_z
 
 
 
 cap = cv2.VideoCapture("C:/Users/willi/Downloads/1.mp4")
-lower_z = 0.5
-upper_z = 1
+upper_z = 200
 ret, old_image = cap.read()
 old_gray_image = cv2.cvtColor(old_image, cv2.COLOR_BGR2GRAY)
 
+new_tracking_points = []
+
 while True:
+
     ret, image = cap.read()
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     if not ret:
         break
 
-    edges, lower_z, upper_z = dynamic_canny(image, lower_z=lower_z, upper_z=upper_z)
+    edges, upper_z = dynamic_canny(image, upper_z=upper_z)
 
     cdst = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-    linesP = cv2.HoughLinesP(edges, 4, np.pi / 180, 500, None, 300, 20)
+    linesP = cv2.HoughLinesP(edges, 50, np.pi / 180, 120, None, 150, 10)
 
     if linesP is not None:
-        new_tracking_points = []
         for i in range(0, len(linesP)):
             l = linesP[i][0]
             cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
@@ -76,11 +78,14 @@ while True:
         
         p0 = np.array(new_tracking_points, dtype=np.float32).reshape(-1, 1, 2)
         p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray_image, gray_image, p0, None)
-        
+
+        tracking_points2 = []
         for i in range(len(p1)):
             cords = (int(p1[i][0,0]), int(p1[i][0,1]))
             cv2.arrowedLine(image, new_tracking_points[i], cords, (0,0,225), 4)
+            tracking_points2.append(cords)
         old_gray_image = gray_image
+        new_tracking_points = tracking_points2
     cv2.imshow("Optical Flow", image)
     cv2.imshow("Detected Lines (in red) - Probabilistic Hough Transform", cdst)
     cv2.waitKey(1)
